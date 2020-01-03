428ec8ca9fe7042a4cfa7e8c45670e5d896b260c
farid.abdalla <farid.abdalla@he-arc.ch>
2020-01-03 15:19:04 +0100

    No more displaying empty informations when requesting user informations

--- app/src/main/java/ch/hesso/keepin/pojos/PublicUser.kt
+++ app/src/main/java/ch/hesso/keepin/pojos/PublicUser.kt
@@ -5,5 +5,5 @@ import ch.hesso.keepin.enums.Status
 
 data class PublicUser(val endpointId : String,
                       val name: String,
-                      val status: Status,
+                      var status: Status,
                       val lastName: String = "")
